326f05ca1d211b42b185dc0ac5299f25899b87c8
Lucas <tech@firebrandwebsolutions.com>
2019-09-21 19:10:41 -0700

    Update home screen text.

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
         <ion-buttons slot="start" class="sc-ion-buttons-ios-h sc-ion-buttons-ios-s ios buttons-first-slot hydrated">
           <ion-menu-button class="hydrated ios button ion-activatable ion-focusable activated"></ion-menu-button>
         </ion-buttons>
-        <ion-title class="ios title-ios hydrated">Home</ion-title>
+        <ion-title class="ios title-ios hydrated">Get Started</ion-title>
       </ion-toolbar>
       <ion-router-outlet id="page-wrapper"></ion-router-outlet>
     </ion-header>
