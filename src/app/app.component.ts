bc9a6bd427c72628c0dc1befa8398d5f551343b8
Lucas <tech@firebrandwebsolutions.com>
2019-09-21 18:59:56 -0700

    Add menu.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,121 @@
 import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   template: `
     <ion-app>
-      <ion-router-outlet></ion-router-outlet>
+      <ion-menu contentId="content">
+        <ion-header>
+          <ion-toolbar color="primary">
+            <ion-title>EDX Aries Client</ion-title>
+          </ion-toolbar>
+        </ion-header>
+
+        <ion-content>
+
+          <div *ngFor="let p of pages">
+
+            <!-- Standard Menu Item -->
+            <ion-menu-toggle *ngIf="p.url" autoHide="false">
+              <ion-item [routerLink]="p.url" routerDirection="root" routerLinkActive="active">
+                <ion-icon [name]="p.icon" slot="start"></ion-icon>
+                <ion-label>
+                  {{ p.title }}
+                </ion-label>
+              </ion-item>
+            </ion-menu-toggle>
+            <!-- Item with Children -->
+
+            <ion-item button *ngIf="p.children?.length > 0" (click)="p.open = !p.open" [class.parent-active]="p.open" detail="false">
+              <ion-icon slot="start" name="arrow-forward" *ngIf="!p.open"></ion-icon>
+              <ion-icon slot="start" name="arrow-down" *ngIf="p.open"></ion-icon>
+              <ion-label>{{ p.title }}</ion-label>
+            </ion-item>
+
+            <!-- Children List for clicked Item -->
+            <ion-list *ngIf="p.open">
+              <ion-menu-toggle>
+                <ion-item *ngFor="let sub of p.children" class="sub-item" [routerLink]="sub.url" routerDirection="root"
+                  routerLinkActive="active">
+                  <ion-icon [name]="sub.icon" slot="start"></ion-icon>
+                  <ion-label>
+                    {{ sub.title }}
+                  </ion-label>
+                </ion-item>
+              </ion-menu-toggle>
+            </ion-list>
+
+          </div>
+        </ion-content>
+
+      </ion-menu>
+
+      <ion-router-outlet id="content" main></ion-router-outlet>
     </ion-app>
   `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'edx-ariescloud-client';
+
+  pages = [
+    {
+      title: 'My Profile',
+      url: '/menu/',
+      // icon: 'home'
+      children: []
+    },
+    {
+      title: 'My Relationships',
+      children: [
+        {
+          title: 'Create Relationship',
+          url: '/menu/',
+          // icon: 'logo-ionic'
+        },
+        {
+          title: 'Pending Connections',
+          url: '/menu/',
+          // icon: 'logo-ionic'
+        }
+      ]
+    },
+    {
+      title: 'My Credentials',
+      children: [
+        {
+          title: 'Create Credential',
+          url: '/menu/',
+          // icon: 'logo-ionic'
+        },
+        {
+          title: 'Credentials Received',
+          url: '/menu/',
+          // icon: 'logo-ionic'
+        },
+        {
+          title: 'Credentials Owned',
+          url: '/menu/',
+          // icon: 'logo-google'
+        }
+      ]
+    }
+  ];
+
+  constructor(private menu: MenuController) {}
+
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
+
+  openEnd() {
+    this.menu.open('end');
+  }
+
+  openCustom() {
+    this.menu.enable(true, 'custom');
+    this.menu.open('custom');
+  }
 }
--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -3,8 +3,15 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 @Component({
   selector: 'app-home',
   template: `
-    <nav>zzz</nav>
-    abcdef
+    <ion-header role="banner" class="ios header-ios hydrated">
+      <ion-toolbar class="ios hydrated">
+        <ion-buttons slot="start" class="sc-ion-buttons-ios-h sc-ion-buttons-ios-s ios buttons-first-slot hydrated">
+          <ion-menu-button class="hydrated ios button ion-activatable ion-focusable activated"></ion-menu-button>
+        </ion-buttons>
+        <ion-title class="ios title-ios hydrated">Home</ion-title>
+      </ion-toolbar>
+      <ion-router-outlet id="page-wrapper"></ion-router-outlet>
+    </ion-header>
   `,
   styleUrls: ['./home.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
