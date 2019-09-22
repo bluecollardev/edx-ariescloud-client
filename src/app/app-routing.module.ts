3a9f544cc03fec571f64927aa35f2e9493ed254a
Hamilton <Sean.Hamilton@quartech.com>
2019-09-21 19:22:57 -0700

    updated routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+// tslint:disable-next-line: max-line-length
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'relationships',
+    loadChildren: `./relationships/relationship.module#RelationshipsModule`
+  }
+];
 
 @NgModule({
   declarations: [],
--- src/app/relationships/relationships-routing.module.ts
+++ src/app/relationships/relationships-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RelationshipsComponent } from './relationships.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: RelationshipsComponent,
+    children: [
+      {
+        path: '',
+        component: RelationshipsComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class RelationshipsRoutingModule { }
+export class RelationshipsRoutingModule {}
