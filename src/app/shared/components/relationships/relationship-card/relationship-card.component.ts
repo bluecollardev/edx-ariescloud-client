0145570e732b70e989a2d3956b81c10a4c3517f5
Lucas <tech@firebrandwebsolutions.com>
2019-09-22 15:59:22 -0700

    Fix import path.

---
+++ src/app/shared/components/relationships/relationship-card/relationship-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-relationship-card',
+  templateUrl: './relationship-card.component.html',
+  styleUrls: ['./relationship-card.component.css']
+})
+export class RelationshipCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
